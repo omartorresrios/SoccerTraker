87b68e5380cf5aa599b44bf5f18e972a1ff53c66
Omar Torres <torresomar44@gmail.com>
2019-01-02 17:58:19 -0500

    Initial Commit

---
+++ SoccerTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoccerTracker
+//
+//  Created by Omar Torres on 1/2/19.
+//  Copyright © 2019 OmarTorres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
